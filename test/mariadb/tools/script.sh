d02e898a0b22c7ef9d2f88d553ab4996959896ed
jlim <jlim@42seoul.kr>
2022-09-20 14:36:16 +0900

    test

--- test/mariadb/tools/script.sh
+++ test/mariadb/tools/script.sh
@@ -4,15 +4,15 @@ cat .setup
 
 if [ $? -ne 0 ]; then
 	/usr/bin/mysqld_safe --datadir=/var/lib/mysql &
-	sed -i "s/skip-networking/# skip-networking/g" /etc/my.cnf.d/mariadb-server.cnf
-	sed -i "s/.*bind-address\s*=.*/bind-address=0.0.0.0\nport=3306/g" /etc/my.cnf.d/mariadb-server.cnf
+	# sed -i "s/skip-networking/# skip-networking/g" /etc/my.cnf.d/mariadb-server.cnf
+	# sed -i "s/.*bind-address\s*=.*/bind-address=0.0.0.0\nport=3306/g" /etc/my.cnf.d/mariadb-server.cnf
 
 	if ! mysqladmin --wait=30 ping; then
 		printf "MariaDB Daemon Unreachable\n"
 		exit 1
 	fi
 
-	eval "echo \"$(cat /tmp/query.sql)\"" | mariadb
+	# eval "echo \"$(cat /tmp/query.sql)\"" | mariadb
 	mysqladmin --user=root --password=abcd shutdown
 	touch .setup
 fi
